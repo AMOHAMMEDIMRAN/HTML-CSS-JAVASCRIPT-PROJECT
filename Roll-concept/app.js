465524615adc1303be8c7b2a9802d0b063b6e017
AMOHAMMEDIMRAN <amohammedimran.11@gmail.com>
2024-05-19 12:13:34 +0530

    Roll concept

---
+++ Roll-concept/app.js
@@ -0,0 +1,9 @@
+const resultValue = document.querySelector("#result");
+
+document.querySelector("#dice").addEventListener("click" , function roll(){
+    
+    const calc = Math.round(Math.random()*6 +1)
+    resultValue.textContent = calc
+    console.log(calc);
+  
+})
